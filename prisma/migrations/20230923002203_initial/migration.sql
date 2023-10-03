8401c0527003eb02d9faa378d36609d1ae285ea8
sam <slcaroquiroga@gmail.com>
2023-10-03 09:40:46 -0500

    migracion inicial

--- prisma/migrations/20230923002203_initial/migration.sql
+++ prisma/migrations/20230923002203_initial/migration.sql
@@ -2,35 +2,36 @@
 CREATE TYPE "Role" AS ENUM ('Admin', 'Waiter', 'Chef');
 
 -- CreateEnum
-CREATE TYPE "orders_status" AS ENUM ('Pending', 'Canceled', 'Delivering', 'Delivered');
+CREATE TYPE "OrdersStatus" AS ENUM ('Pending', 'Canceled', 'Delivering', 'Delivered');
 
 -- CreateTable
-CREATE TABLE "users" (
+CREATE TABLE "Users" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL,
+    "Role" "Role" NOT NULL,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "orders" (
+CREATE TABLE "Orders" (
     "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "UserId" INTEGER NOT NULL,
     "client" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
 
-    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Orders_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "orders_products" (
+CREATE TABLE "Orders_Products" (
     "id" SERIAL NOT NULL,
-    "orderId" INTEGER NOT NULL,
-    "productId" INTEGER NOT NULL,
+    "OrderId" INTEGER NOT NULL,
+    "ProductId" INTEGER NOT NULL,
     "qty" INTEGER NOT NULL,
 
-    CONSTRAINT "orders_products_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "OrdersProducts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -47,19 +48,19 @@ CREATE TABLE "Products" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "orders_userId_key" ON "orders"("userId");
+CREATE UNIQUE INDEX "Orders_UserId_key" ON "Orders"("UserId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Products_name_key" ON "Products"("name");
 
 -- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Orders" ADD CONSTRAINT "Orders_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "orders_products" ADD CONSTRAINT "orders_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Orders_Products" ADD CONSTRAINT "Orders_Products_ProductId_fkey" FOREIGN KEY ("ProductId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "orders_products" ADD CONSTRAINT "orders_products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Orders_Products" ADD CONSTRAINT "Orders_Products_OrderId_fkey" FOREIGN KEY ("OrderId") REFERENCES "Orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
